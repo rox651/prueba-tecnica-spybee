fbde9824420185b92aef913c0a51a3c6b6565c7f
homero <homerojose69@gmail.com>
2025-02-06 13:41:07 -0500

    fix: add suspense component on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import styles from "./page.module.css";
 
 import { Projects } from "@/features/projects/components/Projects";
-
+import { Suspense } from "react";
 export default function Home() {
    return (
       <div className={styles.page}>
-         <Projects />
+         <Suspense fallback={<div>Loading...</div>}>
+            <Projects />
+         </Suspense>
       </div>
    );
 }
